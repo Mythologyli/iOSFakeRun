73c6d32928596566c34cbb8ff9d4fba3da126431
Myth <lxbpxylps@126.com>
2023-10-27 11:11:57 +0800

    feat: bump version to 0.9

--- iOSFakeRun/MainWindow.xaml.cs
+++ iOSFakeRun/MainWindow.xaml.cs
@@ -286,6 +286,6 @@ public partial class MainWindow
 
     private void About(object sender, RoutedEventArgs e)
     {
-        MessageBox.Show("iOS Fake Run\n版本: v0.8\n作者: Myth\n\n请勿将本工具用于任何非法用途");
+        MessageBox.Show("iOS Fake Run\n版本: v0.9\n作者: Myth\n\n请勿将本工具用于任何非法用途");
     }
 }
